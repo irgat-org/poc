7f249d13dde9ce9687533e8ac86603aaa851f93b
Ekrem Altınel <ekrem6nel@gmail.com>
2017-02-06 23:18:33 +0200

    Test for auto-redeployment.

--- src/main/java/com/irgat/rest/HelloWorldEndpoint.java
+++ src/main/java/com/irgat/rest/HelloWorldEndpoint.java
@@ -8,6 +8,6 @@ public class HelloWorldEndpoint
 {
     @RequestMapping("/helloWorld")
     public String helloWorld() {
-        return "Hello World!";
+        return "Hello World after redeployment!";
     }
 }
